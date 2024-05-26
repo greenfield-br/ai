3f83fede623d9eceed106497d39f54ed53196e52
operationa <greenfield.tree@gmail.com>
2024-05-25 22:26:18 -0300

    nothing worth commenting.

--- annGate.py
+++ annGate.py
@@ -9,12 +9,12 @@ class AN:
 		self.A = 0.5 * ones(_N)	#array of zeros as default weights
 		self.u = 0.5			#bias = 0 as default
 
-	def f(self, _arg=None, _rate=1, _zone=[0, 1], _target='y'):
-		_z = 0
+	def f(self, _arg=None, _rate=1, _zone=[0, 1], _target='y'):					#_target = dJ, means you're on a backprop. output is f'(z)
+		_z = 0																	#		 = y , means you are evaluating current output 
 		if isinstance(_arg, int):     _z = _arg									#either z is given as parameter
 		if isinstance(_arg, float):   _z = _arg							
 		if isinstance(_arg, list):    _z = dot(self.A, array(_arg)) + self.u	#or it is calculated through _arg
-		if isinstance(_arg, ndarray): _z = dot(self.A, array(_arg)) + self.u
+		if isinstance(_arg, ndarray): _z = dot(self.A, _arg) + self.u
 		y = 0
 		if _z > _zone[0]:
 			y = _rate * (_z - _zone[0])
@@ -28,8 +28,9 @@ class AN:
 		return y
 
 	def Y(self, _X):
-		z = dot(self.A, array(_X)) + self.u
-		y = self.f(z)
+		if isinstance(_X, list):    _z = dot(self.A, array(_X)) + self.u
+		if isinstance(_X, ndarray): _z = dot(self.A, _X) + self.u
+		y = self.f(_z)
 		return y
 
 
@@ -50,7 +51,7 @@ class ANN:
 			retCode = 0
 		if not all(isinstance(i, int) for i in _KN):
 			retCode = 0
-		if retCode != 1: #exit if failed, as expected.
+		if retCode != 1:	#exit if failed, as expected.
 			return retCode
 		#array structure tests
 		arrKN = array(_KN)
@@ -65,8 +66,8 @@ class ANN:
 			retCode = 0
 		if retCode != 1:
 			return retCode
-		_KN.append(_out) #_KN is now extended to the total number of layers
-		self.KN = _KN #it does not need to be an array
+		_KN.append(_out)	#_KN is now extended to the total number of layers
+		self.KN = _KN		#it does not need to be an array
 		#ANN building as list of lists of AN class instances
 		lstANN = []
 		lenKN = len(_KN)
@@ -133,8 +134,6 @@ def gradJ(_ann, _X, _hatY):
 		dJ[counter1][-1] *= 2 * e[counter1] * 1
 	return dJ, dot(e, e)
 
-
-
 def iterBackprop(_ann, _lrnCoef, _lstX, _lstHatY):
 	lenK = len(_ann.lst[-1])						#number of neurons on output layer
 	_A = [_ann.lst[-1][i].A for i in range(lenK)]
@@ -143,7 +142,7 @@ def iterBackprop(_ann, _lrnCoef, _lstX, _lstHatY):
 	J = 0
 	lenLstX = len(_lstX)
 	for counter in range(lenLstX):
-		[dj, j] = gradJ(_ann, _lstX[counter], _lstHatY[counter])
+		[dj, j] = gradJ2(_ann, _lstX[counter], _lstHatY[counter])
 		dJ += dj
 		J  += j
 	dJ /= lenLstX
@@ -163,15 +162,50 @@ def backprop(_ann, _lrnCoef, _iterN, _mode = 'silent'):
 	return
 
 
+
+#x = ANN(AN(2), 2, 2, [3, 3])
+
+#
+# need to rewrite gradJ according to matricial structure.
+#
+
+def gradJ2(_ann, _X, _hatY, _k=1):
+	hatY = array(_hatY)
+	lstY = _ann.Y(_X)		#feedforward input
+	lyrX = array(lstY[-2])	#input values are outputs of previous layer
+	lyrY = array(lstY[-1])	#output is current layer neuron output list
+	e = lyrY - hatY
+	lenK = len(lstY)
+	Nin = len(lyrX)
+	Nk = len(lyrY)
+	dJ = zeros([Nk, Nin+1])
+	
+	#rewrite this for loop where lyrX[i] is the split point for parallelization, thus
+	#rewrite it for looping over all the layer neurons under the same input parameter, and then
+	#               looping over all the layer neurons under other    input parameter.
+	for counter1 in range(Nin):
+		for counter2 in range(Nk):
+			dJ[counter2][counter1]  = _ann.lst[-1][counter2].f(lyrX, _target='dJ')	#f() derivative
+			dJ[counter2][counter1] *= 2 * e[counter2] * lyrX[counter1]					#dJ module
+	for counter in range(Nk):
+		dJ[counter][-1]  = _ann.lst[-1][counter].f(lyrX, _target='dJ')
+		dJ[counter][-1] *= 2 * e[counter] * 1
+	return dJ, dot(e, e)
+
 x = ANN(AN(2), 2, 2, [3, 3])
-#a = x.Y([1, 1])
-#b = x.lstGet('A')
-#a, b = gradJ(x, [1, 1], [0, 1])
+y = x.Y([1, 1])
+print(y)
+
 a, b = iterBackprop(x, 0.1, [[1, 1]], [[0, 1]])
 print(a)
 print(b)
-print(x.lstGet('u'))
+y = x.Y([1, 1])
+print(y)
+
 a, b = iterBackprop(x, 0.1, [[1, 1]], [[0, 1]])
 print(a)
 print(b)
-print(x.lstGet('u'))
+y = x.Y([1, 1])
+print(y)
+
+#now i need to check again against google sheet.
